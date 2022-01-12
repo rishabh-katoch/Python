ebdcae59c4609fdadcf76fe69ae643ad48d9a13d
Rishabh Katoch <katoch.rishabh@outlook.com>
2022-01-12 08:24:16 +0530

    added a comment for loop and texts

--- demo.py
+++ demo.py
@@ -1,3 +1,14 @@
 print("hello")
 print("test")
 print(3+4)
+
+greeting = 'Hello'
+name = ['Rishabh','Akshay','Ashok']
+
+for text in name:
+    message = f'{greeting}, {text} Welcome!'
+    print(message)
+
+for text in name:
+    message = '{}, {} Welcome!'.format(greeting, text)
+    print(message)
