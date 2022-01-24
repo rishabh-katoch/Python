1a33fad4c174717e7486bba1e7a4edb5578a61a0
Rishabh Katoch <katoch.rishabh@outlook.com>
2022-01-24 09:19:06 +0530

    added for loop

--- demo1.py
+++ demo1.py
@@ -4,3 +4,9 @@ if age > 21:
     print("You are eligible for beer")
 else:
     print("You are underage")
+
+# for loop
+food = ['fish', 'chapati', 'burger', 'chinese', 'continental']
+
+for f in food:
+    print("I love", f, " which has ", len(f) , " characters")
