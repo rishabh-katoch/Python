53e5bd1e6399c1fd12dff99be1ea64a10473b455
Rishabh Katoch <katoch.rishabh@outlook.com>
2022-01-25 07:08:43 +0530

    write numbers less than the number entered divided by 100

--- day - 3.py
+++ day - 3.py
@@ -10,3 +10,14 @@ for n in range(101):
 '''
 comments can be added by us3ing the three aposthrephe
 '''
+
+# write numbers less than the number entered divided by 100
+
+num = int(input("Enter a number:"))
+for x in range (num +1):
+    if x%4 == 0:
+        print(x)
+        break
+    else:
+        print("No numbers found")
+
