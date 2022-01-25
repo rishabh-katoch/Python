248d4a8e5ccdad14bed8d5700e0cff26ee0e2e3a
Rishabh Katoch <katoch.rishabh@outlook.com>
2022-01-25 07:02:56 +0530

    worked on comments and break

---
+++ day - 3.py
@@ -0,0 +1,12 @@
+# comments and break
+
+number = int(input("Enter a number:"))
+for n in range(101):
+    if n is number:
+        print(n, "is the number that was entered")
+        break
+    else:
+        print(n)
+'''
+comments can be added by us3ing the three aposthrephe
+'''
