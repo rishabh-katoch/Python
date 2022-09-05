65ed1daa229118e242e1f0b1111fd421611ff414
Rishabh <48093390+rishabh-katoch@users.noreply.github.com>
2022-09-05 23:39:02 +0000

    added a new code with a debugging work

---
+++ main.py
@@ -0,0 +1,21 @@
+# day 1 
+# Write a program in main.py that prints the same notes from the previous lesson using what you have learnt about the Python print function.
+# Example Output After you have written your code, you should run your program and it should print the following:
+# Example Output 
+
+print("Day 1 - Python Print Function \n The function is declared like this: \n print('what to print')")
+
+# Look at the code in the code editor on the right. There are errors in all of the lines of code. Fix the code so that it runs without errors.
+
+#Fix the code below 👇
+
+#print(Day 1 - String Manipulation")
+#print("String Concatenation is done with the "+" sign.")
+#  print('e.g. print("Hello " + "world")')
+#print(("New lines can be created with a backslash and n.")
+
+#fixed Code      
+print("Day 1 - String Manipulation")
+print('''String Concatenation is done with the "+" sign.''')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
