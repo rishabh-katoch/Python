27ff97d0ea4476bbd93a559689588b2df89bbfe6
Rishabh <48093390+rishabh-katoch@users.noreply.github.com>
2022-09-05 23:18:22 +0000

    Added the first code from day 1

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hello World")
+# day 1 
+# Write a program in main.py that prints the same notes from the previous lesson using what you have learnt about the Python print function.
+# Example Output After you have written your code, you should run your program and it should print the following:
+# Example Output 
+
+print("Day 1 - Python Print Function \n The function is declared like this: \n print('what to print')")
