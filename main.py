1530b220e814e90c983d3f354ad5f43686fe7566
Rishabh <48093390+rishabh-katoch@users.noreply.github.com>
2022-09-06 03:23:28 +0000

    Write a program that switches the values stored in the variables a and b.

--- main.py
+++ main.py
@@ -19,3 +19,27 @@ print("Day 1 - String Manipulation")
 print('''String Concatenation is done with the "+" sign.''')
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
+
+# program to find length of name
+a = input("what is your name ? : \n")
+print(len(a))
+
+# Write a program that switches the values stored in the variables a and b.
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+c = a 
+a = b
+b = c 
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
