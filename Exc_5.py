81e20f676c9df1b14437732488a73ea8f03b44f1
Rishabh <48093390+rishabh-katoch@users.noreply.github.com>
2022-09-11 04:33:34 +0000

    Day 3 Final Project Treasure Island

--- Exc_5.py
+++ Exc_5.py
@@ -1,4 +1,4 @@
-# 🚨 Don't change the code below 👇
+ # 🚨 Don't change the code below 👇
 print("Welcome to the Love Calculator!")
 name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
